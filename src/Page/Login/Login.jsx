36117497c495af13a96510ba0971397c69741ace
sohagroy7223 <freelancersohagroy@gmail.com>
2026-03-16 13:08:39 +0600

    addad login form and register form in this page

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
+  const handelLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h3>this is login page</h3>
+    <div className="card bg-base-100 w-full mx-auto mt-14 max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-4xl text-center font-bold">Login now!</h1>
+      <div className="card-body">
+        <form onSubmit={handelLogin} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            autoComplete="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            autoComplete="current-password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>
+          create new account?{" "}
+          <Link className="text-blue-500 font-bold underline" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
   return (
-    <div>
-      <h3>this is Register page</h3>
+    <div className="card bg-base-100 w-full mx-auto mt-14 max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-4xl text-center font-bold">Register now!</h1>
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            autoComplete="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            autoComplete="current-password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </form>
+        <p>
+          already have an account?{" "}
+          <Link className="text-blue-500 font-bold underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
